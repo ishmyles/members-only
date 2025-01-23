71d5b43a5fae77368e0312c31413a4991d8a4fde
Myles Gaerlan <ish.myles@gmail.com>
2025-01-23 15:37:07 +1100

    feat: add seedData for app
    add script to add starter data in event the db needs to be reset

---
+++ db/seedData.js
@@ -0,0 +1,68 @@
+import pg from "pg";
+const { Client } = pg;
+
+const sqlQuery = `
+DROP TABLE IF EXISTS "UserSessions";
+DROP TABLE IF EXISTS Messages;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS MemberTypes;
+
+CREATE TABLE "UserSessions" (
+    "sid" VARCHAR NOT NULL COLLATE "default",
+    "sess" JSON NOT NULL,
+    "expire" TIMESTAMP(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "UserSessions" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "UserSessions" ("expire");
+
+CREATE TABLE MemberTypes (
+    id INTEGER PRIMARY KEY, 
+    type VARCHAR(25)
+);
+
+CREATE TABLE Users (
+    username VARCHAR(25) PRIMARY KEY,
+    firstname VARCHAR(25),
+    lastname VARCHAR(25),
+    password VARCHAR,
+    membertype INTEGER,
+    FOREIGN KEY (membertype) REFERENCES MemberTypes (id)
+);
+
+CREATE TABLE Messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(50),
+    text VARCHAR(255),
+    createdAt TIMESTAMP(6) NOT NULL,
+    createdBy VARCHAR(25),
+    FOREIGN KEY (createdBy) REFERENCES Users (username) ON DELETE CASCADE
+);
+
+INSERT INTO MemberTypes (id, type) VALUES 
+(0, 'Admin'),
+(1, 'Guest'),
+(2, 'Member');
+
+SELECT * FROM MemberTypes;
+`;
+
+console.log(process.env.DB_CONN);
+const populateData = async () => {
+  const client = new Client({
+    connectionString: process.env.DB_CONN,
+  });
+
+  console.log("SEEDING...");
+
+  await client.connect();
+  const result = await client.query(sqlQuery);
+  await client.end();
+  console.log(result[result.length - 1].rows);
+
+  console.log("DONE!");
+};
+
+populateData();
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -5,10 +5,25 @@ import {
 } from "../controllers/adminController.js";
 import { adminSecretValidator } from "../utils/formValidator.js";
 
+// Middleware will run even if the validation fails.
+// You will need to add a condition in the middle to stop
+// actions such as querying the DB.
+import { validationResult } from "express-validator";
+const middleware = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log("Validation failed");
+    return next();
+  }
+  console.log("WORKS");
+  next();
+};
+// END OF TEST CODE
+
 const adminRouter = Router();
 
 adminRouter.get("/new", authoriseAdminGet);
 
-adminRouter.post("/new", adminSecretValidator, authoriseAdminPost);
+adminRouter.post("/new", adminSecretValidator, middleware, authoriseAdminPost);
 
 export default adminRouter;
